69be42b8818dad82f6935f04af57b3851a444773
MJSoftKing <1406993111@qq.com>
2021-06-02 15:40:48 +0800

    更新

---
+++ app/src/main/java/com/mjsoftking/tcpserviceapp/test/generate/ServiceDataGenerate.java
@@ -0,0 +1,25 @@
+package com.mjsoftking.tcpserviceapp.test.generate;
+
+import com.mjsoftking.tcplib.dispose.TcpBaseDataGenerate;
+
+import java.nio.charset.Charset;
+
+/**
+ * 用途：
+ * <p>
+ * 作者：MJSoftKing
+ * 时间：2021/06/02
+ */
+public class ServiceDataGenerate implements TcpBaseDataGenerate {
+
+
+    @Override
+    public byte[] generate(String content) {
+        return content.getBytes(Charset.forName("UTF-8"));
+    }
+
+    @Override
+    public byte[] generate(byte[] contentBytes) {
+        return contentBytes;
+    }
+}
